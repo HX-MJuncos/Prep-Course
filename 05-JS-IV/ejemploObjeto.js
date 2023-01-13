707d30edd41a7d97c5130bd2f42b2f5d45169a2c
Carlos Martin Juncos <mjuncos@soyhenry.com>
2023-01-13 09:32:22 -0300

    130120220923

---
+++ 05-JS-IV/ejemploObjeto.js
@@ -0,0 +1,54 @@
+
+var usuarios = [
+    {
+    nombre: "Andres",
+    apellido: 'Alvarez',
+    usuario: 'AndAlv',
+    contraseña: "hola123",
+    edad: 26,
+    direccion: {
+        calle: 'Colon 100',
+        ciudad: 'Cordoba'
+    },
+    online: true,
+    hobbies: ['correr', 'bailar', 'fotografia'],
+    saludar: function(){
+    console.log('hola, Andres');
+    }
+   },
+   {
+    nombre: "Claudia",
+    apellido: 'Zini',
+    usuario: 'ClaZin',
+    contraseña: "chu987",
+    edad: 42,
+    direccion: {
+        calle: 'San Martin 200',
+        ciudad: 'Buenos Aires'
+    },
+    online: true,
+    hobbies: ['entrenar', 'pintar', 'viajar'],
+    saludar: function(){
+    console.log('hola, Claudia');
+    }
+   },
+   {
+    nombre: "Marcelo",
+    apellido: 'Ramirez',
+    usuario: 'MarRam',
+    contraseña: "mus654",
+    edad: 35,
+    direccion: {
+        calle: 'Belgrano 800',
+        ciudad: 'Mendoza'
+    },
+    online: false,
+    hobbies: ['boxeo', 'entrenar', 'Djs'],
+    saludar: function(){
+    console.log('hola, Marcelo');
+    }
+   }  
+]
+
+
+  
